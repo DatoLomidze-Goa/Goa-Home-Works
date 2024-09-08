81506a5f70c594893a51415675e642fc7603fc43
DatoLomidze-Goa <d.lomidze2009@gmail.com>
2024-09-08 17:09:44 +0400

    commit

---
+++ day 11/lesson1.py
@@ -0,0 +1,72 @@
+#დავალება 1:
+num1 =int(input("Please enter first number:"))
+num2 =float(num1)
+
+#დავალება 2:
+print(51 > 3)
+print(34 > 6)
+print(8 > 7)
+print(64 > 51)
+print(41 > 4)
+#დავალება 3:
+print(8 >= 11)
+print(4 >= 45)
+print(3 >= 123)
+print(65 >= 124)
+print(24 >= 156)
+#დავალება 4:
+print(21 < 35)
+print(31 < 32)
+print(51 < 52)
+print(15 < 41)
+print(45 < 54)
+#დავალება 5:
+print(61 <= 21)
+print(11 <= 5)
+print(14 <= 1 )
+print(155 <= 15)
+print(51 <= 44)
+#დავალება 6:
+print(55 == 55)
+print(12 == 12)
+print(5 == 5)
+print(155 == 155)
+print(24512 == 24512)
+#დავალება 7:
+print(5 != 5)
+print(10 != 10)
+print(51 != 51)
+print(42 != 42)
+print (23 != 23)
+#დავალება 8:
+num1 = float(input("Write First Number: "))
+num2 = float(input("Write Second Number: "))
+print(num1 >= 5)
+print(num2 >= 10)
+
+print(5 > 10)
+print(5 >= 10)
+print(5 < 10)
+print(5 <= 10)
+print(5 == 10)
+print(5 != 10)
+
+#დაველება 9:
+number = int(input("Please enter number: "))
+
+number2 = number + 10
+
+print(number > number2)
+print(number < number2)
+print(number >= number2)
+print(number <= number2)
+print(number == number2)
+print(number != number2)
+            
+
+#დავალება 10:
+num1 = int(input("Please enter first number: "))
+num2 = float(input("Please enter second number: "))
+
+print(type(num1) == type(num2))
+print(type(num1) != type(num2))
