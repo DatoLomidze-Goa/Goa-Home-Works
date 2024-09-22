93f227990ba831a95ce3714964b2d75ba0bb84c5
DatoLomidze-Goa <d.lomidze2009@gmail.com>
2024-09-23 02:54:11 +0400

    second commit

---
+++ day 33/lesson33.py
@@ -0,0 +1,37 @@
+#Davaleba 1:
+#You get an array of numbers, return the sum of all of the positives ones.
+
+#Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+#Note: if there is nothing to sum, the sum is default to 0.
+
+
+#def positive_sum(arr):
+#    sum = 0
+#    for e in arr:
+#        if e > 0:
+#            sum = sum + e
+#    return sum
+
+
+#Davaleba 2:
+
+#Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+#For example, for [1, 2, 2] it should return 9 because 
+
+#For example, for [1, 2, 2] it should return 9 
+
+
+#def square_sum(numbers):
+#    return sum(x ** 2 for x in numbers)
+
+
+
+#Davaleba 3:
+
+#Code as fast as you can! You need to double the integer and return it.
+
+
+#def doubleInteger(i):
+#   return i * 2
